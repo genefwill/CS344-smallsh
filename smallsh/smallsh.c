8dbeb6f92507f186ff01a85c0278468f7efeee4c
Genevieve Will <genefwill@gmail.com>
2022-07-19 15:11:59 -0700

    working on prompt command

---
+++ smallsh/smallsh.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <err.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+char* prompt_line() {
+    char* prompt;
+    char* curr_prompt = NULL;
+
+    size_t buffer_size = 0;    
+    printf(": ");
+    fflush(stdout);
+    get_line(&curr_prompt, &buffer_size, stdin);
+    printf("Here is the prompt entered: %s\n", prompt);
+
+    return prompt;
+
+
+}
+
+
+int main (int argc, char* argv[]) {
+
+    prompt_line();
+    return 0;
+
+
+
+
+}
